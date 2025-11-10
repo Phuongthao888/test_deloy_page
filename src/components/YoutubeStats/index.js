67606c9be1a14a3a45b52c31bee13d02626bf092
Phuongthao888 <thaonhilam562@gmail.com>
2025-11-11 01:28:56 +0700

    fix

--- src/components/YoutubeStats/index.js
+++ src/components/YoutubeStats/index.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { motion } from "framer-motion";
 import Header from './Header';
 import TabNavigation from './TabNavigation';
--- src/components/YoutubeStats/user/index_user.js
+++ src/components/YoutubeStats/user/index_user.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { motion } from "framer-motion";
 import VideoListTab_user from './VideoListTab_user';
 import VideoDetailsTab from '../VideoDetailsTab';
